2806c57a6ca611e95fc262411e8cd206fe1dea68
Di Wu <di.wu@me.com>
2024-12-30 15:16:06 +0800

    change messageBox typo

--- src/stories/notice/MessageBox.stories.js
+++ src/stories/notice/MessageBox.stories.js
@@ -5,19 +5,45 @@ Vue.use(ElementUI);
 
 export default {
   title: 'Notice/MessageBox',
-  argTypes: {
-    type: {
-      control: { type: 'select' },
-      options: ['success', 'warning', 'info', 'error'],
-    },
-    customStyle: {
-      control: 'object',
-      defaultValue: {
-        successColor: 'var(--color-success-500)',
-        warningColor: 'var(--color-warning-500)',
-        errorColor: 'var(--color-error-500)',
-        infoColor: 'var(--color-info-500)',
+  parameters: {
+    docs: {
+      description: {
+        component: '消息弹框组件，用于显示确认信息、警告信息、错误信息等。',
       },
     },
   },
-}; 
+};
+
+const Template = () => ({
+  template: `
+    <div>
+      <el-button @click="showAlert">Alert</el-button>
+      <el-button @click="showConfirm">Confirm</el-button>
+      <el-button @click="showPrompt">Prompt</el-button>
+    </div>
+  `,
+  methods: {
+    showAlert() {
+      this.$alert('这是一条消息提示', '标题', {
+        confirmButtonText: '确定',
+      });
+    },
+    showConfirm() {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      });
+    },
+    showPrompt() {
+      this.$prompt('请输入邮箱', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+        inputErrorMessage: '邮箱格式不正确'
+      });
+    },
+  },
+});
+
+export const Default = Template.bind({}); 
