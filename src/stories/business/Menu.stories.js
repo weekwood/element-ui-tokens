35b97587ef2c02298c4e5cb94626e6b9b86a519d
Di Wu <di.wu@me.com>
2025-01-20 14:55:05 +0800

    Refactor Menu.stories.js to remove unused SCSS import. Enhance Tabs.stories.js by adding new tab panes for system settings, permissions, logs, data statistics, message center, workflows, resource management, and audit records. Update element-override.scss to improve styling for navigation elements and vertical menus, ensuring consistent button heights and transitions.

--- src/stories/business/Menu.stories.js
+++ src/stories/business/Menu.stories.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import './Menu.scss';
 
 Vue.use(ElementUI);
 
--- src/stories/navigation/Tabs.stories.js
+++ src/stories/navigation/Tabs.stories.js
@@ -26,6 +26,14 @@ const Template = (args, { argTypes }) => ({
       <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
       <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
       <el-tab-pane label="定时任务" name="fourth">定时任务</el-tab-pane>
+      <el-tab-pane label="系统设置" name="fifth">系统设置</el-tab-pane>
+      <el-tab-pane label="权限管理" name="sixth">权限管理</el-tab-pane>
+      <el-tab-pane label="日志监控" name="seventh">日志监控</el-tab-pane>
+      <el-tab-pane label="数据统计" name="eighth">数据统计</el-tab-pane>
+      <el-tab-pane label="消息中心" name="ninth">消息中心</el-tab-pane>
+      <el-tab-pane label="工作流程" name="tenth">工作流程</el-tab-pane>
+      <el-tab-pane label="资源管理" name="eleventh">资源管理</el-tab-pane>
+      <el-tab-pane label="审计记录" name="twelfth">审计记录</el-tab-pane>
     </el-tabs>
   `,
 });
